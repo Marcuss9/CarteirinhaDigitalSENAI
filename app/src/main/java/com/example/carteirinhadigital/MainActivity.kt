353715575db74c5646609e18095db746ac47419a
Marcus Cruz <mvcruz0907@gmail.com>
2026-02-20 10:31:08 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/carteirinhadigital/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.carteirinhadigital
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.carteirinhadigital.ui.theme.CarteirinhaDigitalTheme
+import com.rafaelcosta.myapplication.QrCode
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CarteirinhaDigitalTheme() {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CarteirinhaDeEstudante(modifier = Modifier.padding(innerPadding).fillMaxSize())
+                }
+            }
+        }
+    }
+}
+
+//diz que a função vai construir algo na tela (retirando o erro de "Column")
+@Composable
+fun CarteirinhaDeEstudante(modifier: Modifier = Modifier){ //nome: tipo, padrao
+    Column (modifier, verticalArrangement = Arrangement.SpaceAround){
+        Image(painter = painterResource(R.drawable.senai_s_o_paulo_logo), contentDescription = "logo_senai_sp")
+        Row {
+            Text(text = "Nome: ", fontSize = 15.sp, fontWeight = FontWeight.Bold)
+            Text(text = "Vierinha", fontSize = 20.sp)
+        }
+        Row {
+            Text(text = "Curso: ", fontSize = 15.sp, fontWeight = FontWeight.Bold)
+            Text(text = "Desenvolvimento de Sistemas", fontSize = 20.sp)
+        }
+
+        QrCode(
+            conteudo = "vierinhadamassa"
+        )
+
+    }
+}
